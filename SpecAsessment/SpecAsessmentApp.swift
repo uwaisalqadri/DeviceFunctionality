b24f8886ddcb724afb66b482b0aa5e2370f83aec
Uwais Alqadri <uwaisalqadri654321@gmail.com>
2023-12-13 09:18:11 +0700

    Initial Commit

---
+++ SpecAsessment/SpecAsessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpecAsessmentApp.swift
+//  SpecAsessment
+//
+//  Created by Uwais Alqadri on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SpecAsessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
